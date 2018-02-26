e9eb7fdfdc1a19548af1f43a421ae1cbf96041da
Jobby00 <jobby00@gmail.com>
2018-02-26 04:01:01 +0100

    Les Entités: BILLETS, RESERVATION et Les Forms Type: BILLETSTYPE, RESERVATIONTYPE

---
+++ src/JD/LouvreBundle/Entity/Reservation.php
@@ -0,0 +1,236 @@
+<?php
+namespace JD\LouvreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Resrvation
+ *
+ * @ORM\Table(name="reservation")
+ * @ORM\Entity(repositoryClass="JD\LouvreBundle\Repository\ReservationRepository")
+ */
+class Reservation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="datecreate", type="datetime")
+     * @Assert\DateTime()
+     * @Assert\GreaterThanOrEqual("today")
+     */
+    private $datecreate;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email = '';
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nbBillets", type="integer")
+     * @ASSERT\Range(
+     *     min = 1,
+     *     max = 20
+     * )
+     */
+    private $nbBillets = 1;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="serial_code", type="string")
+     */
+    private $serialCode;
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="prixTotal", type="float")
+     */
+    private $prixTotal = 0;
+    /**
+     * @ORM\OneToMany(targetEntity="JD\LouvreBundle\Entity\Billets", mappedBy="reservation")
+     * @Assert\Valid
+     */
+    private $billets;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set datecreate
+     *
+     * @param \DateTime $datecreate
+     *
+     * @return Reservation
+     */
+    public function setDatecreate($datecreate)
+    {
+        $this->datecreate = $datecreate;
+
+        return $this;
+    }
+
+    /**
+     * Get datecreate
+     *
+     * @return \DateTime
+     */
+    public function getDatecreate()
+    {
+        return $this->datecreate;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Reservation
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set nbBillets
+     *
+     * @param integer $nbBillets
+     *
+     * @return Reservation
+     */
+    public function setNbBillets($nbBillets)
+    {
+        $this->nbBillets = $nbBillets;
+
+        return $this;
+    }
+
+    /**
+     * Get nbBillets
+     *
+     * @return integer
+     */
+    public function getNbBillets()
+    {
+        return $this->nbBillets;
+    }
+
+    /**
+     * Set serialCode
+     *
+     * @param string $serialCode
+     *
+     * @return Reservation
+     */
+    public function setSerialCode($serialCode)
+    {
+        $this->serialCode = $serialCode;
+
+        return $this;
+    }
+
+    /**
+     * Get serialCode
+     *
+     * @return string
+     */
+    public function getSerialCode()
+    {
+        return $this->serialCode;
+    }
+
+    /**
+     * Set prixTotal
+     *
+     * @param float $prixTotal
+     *
+     * @return Reservation
+     */
+    public function setPrixTotal($prixTotal)
+    {
+        $this->prixTotal = $prixTotal;
+
+        return $this;
+    }
+
+    /**
+     * Get prixTotal
+     *
+     * @return float
+     */
+    public function getPrixTotal()
+    {
+        return $this->prixTotal;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->billets = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add billet
+     *
+     * @param \JD\LouvreBundle\Entity\Billets $billet
+     *
+     * @return Reservation
+     */
+    public function addBillet(\JD\LouvreBundle\Entity\Billets $billet)
+    {
+        $this->billets[] = $billet;
+
+        return $this;
+    }
+
+    /**
+     * Remove billet
+     *
+     * @param \JD\LouvreBundle\Entity\Billets $billet
+     */
+    public function removeBillet(\JD\LouvreBundle\Entity\Billets $billet)
+    {
+        $this->billets->removeElement($billet);
+    }
+
+    /**
+     * Get billets
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getBillets()
+    {
+        return $this->billets;
+    }
+}
